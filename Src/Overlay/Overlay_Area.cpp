1c747e5628f475e61ec1fe3cf3405239705a5789
Bonnqiam <qiamking@163.com>
2024-11-18 17:08:38 +0800

    ✨ (Overlay_Area.cpp, Scanline.hpp): introduce new functionality to calculate the density of the intersection area of two rectangles using the sweep line algorithm. This allows for efficient processing of window data and enhances the application's capability to analyze geometric intersections.

---
+++ Src/Overlay/Overlay_Area.cpp
@@ -0,0 +1,170 @@
+/*
+* Input: two windows consist of rectangles
+* Output: the density of the intersection area of the two windows
+* Algorithm: sweep line algorithm
+*/
+
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <regex>
+#include <stack>
+#include <algorithm>
+#include <time.h>
+
+#include "Scanline.hpp"
+
+#define FILEEND 1
+#define grid_size 20000
+
+int LoadWindowData(std::ifstream &file, Seg *List_Edge, double *List_X_Coordinate, int &num);
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <filename>\n";
+        return 1;
+    }
+
+    std::ifstream file1(argv[1]);
+    std::ifstream file2(argv[2]);
+    std::ofstream file3(argv[3]);
+
+    int file_end1 = 0, file_end2 = 0;
+
+    int Count_grid = 0;
+
+    while (file_end1 == 0 || file_end2 == 0)
+    //while (file_end1 == 0)
+    {
+        Count_grid++;
+        clock_t start, end;
+
+        start = clock();
+
+        int num = 0;
+        mem(t, 0);
+
+        file_end1 = LoadWindowData(file1, e, X, num);
+        file_end2 = LoadWindowData(file2, e, X, num);
+
+        //std::cout << "file_end1 is " << file_end1 << std::endl;
+        //std::cout << "file_end2 is " << file_end2 << std::endl;
+        //std::cout << "Operate at the " << Count_grid << " grid" << std::endl;
+
+#if 1
+
+        // sweep line algorithm
+        std::sort(X, X + num);
+        std::sort(e, e + num);
+
+        int m = std::unique(X, X + num) - X;
+
+        double area = 0;                          // the area of the intersection of the rectangles
+        std::vector<edge<int>> Final_Edge_list;   // The edge collection of the intersection area
+        std::vector<edge<int>> Check_Edge_list;   // effective edge list in the last scanning line
+        std::vector<edge<int>> Checked_Edge_list; // effective edge list in the now scanning line
+
+        for (int i = 0; i < num; i++)
+        {
+            //std::cout << "i is " << i << " in " << num << std::endl;
+
+            int l = std::lower_bound(X, X + m, e[i].l) - X;
+            int r = std::lower_bound(X, X + m, e[i].r) - X - 1;
+
+            if (r == -1)
+                continue;
+
+            update(l, r, 0, m, 1, e[i].f);
+
+            area += t[1].s * (e[i + 1].h - e[i].h);
+        }
+
+        double density = area / (grid_size * grid_size);
+
+        end = clock();
+        std::cout << "Scanning time is " << (double)(end - start) / CLOCKS_PER_SEC / 60 << " min" << " for " << Count_grid << " grid" << std::endl;
+
+        std::cout << "Density is " << density << " for " << Count_grid << " grid" << std::endl;
+        std::cout << "===============================================================" << std::endl;
+
+        // output the result to the file
+        file3 << density << std::endl;
+#endif
+    }
+
+    // close the file
+    file1.close();
+    file2.close();
+    file3.close();
+
+    return 0;
+}
+
+int LoadWindowData(std::ifstream &file, Seg *List_Edge, double *List_X_Coordinate, int &num)
+{
+
+    // check if the file is at the end
+    if (file.eof())
+        return FILEEND;
+
+    std::string line;
+    std::regex re(R"(\((\d+), (\d+)\),\((\d+), (\d+)\))");
+
+    while (std::getline(file, line))
+    {
+        // check if line = '<grid>'
+        if (line == "<grid>")
+        {
+            // read next line
+            std::getline(file, line);
+            //double density = std::stod(line);
+            //std::cout << "Density is " << density << std::endl;
+        }
+        else
+        {
+            // check if line = '</grid>'
+            if (line == "</grid>")
+                break;
+
+            // extract x1,y1,x2,y2 from the format like "(15705, 18196),(16117, 17652)"
+            double x1, y1, x2, y2;
+            std::smatch match;
+            if (std::regex_search(line, match, re))
+            {
+                x1 = std::stod(match[1]);
+                y1 = std::stod(match[2]);
+                x2 = std::stod(match[3]);
+                y2 = std::stod(match[4]);
+            }
+
+            if(x1 == x2 || y1 == y2){
+                //std::cout << "Test" << std::endl;
+                continue;
+            }
+
+            double temp;
+            if (y1 > y2)
+            {
+                temp = y1;
+                y1 = y2;
+                y2 = temp;
+            }
+
+            if (x1 > x2)
+            {
+                temp = x1;
+                x1 = x2;
+                x2 = temp;
+            }
+
+            X[num] = x1;
+            e[num++] = Seg(x1, x2, y1, 1);
+            X[num] = x2;
+            e[num++] = Seg(x1, x2, y2, -1);
+        }
+    }
+
+    return file.eof(); // return 0 if the file is not at the end
+}
