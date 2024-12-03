f61f656739804a4a32c6b0b1d7e7783d8d0702f3
Bonnqiam <qiamking@163.com>
2024-12-03 10:20:06 +0800

    📝 (Execute.sh): add commented-out test commands for Grid_Clean outputs to provide reference for future testing

--- Src/Grid/Execute.sh
+++ Src/Grid/Execute.sh
@@ -2,4 +2,12 @@
 
 #./Test /home/qiamking/Github/Trivial_Dummy/Output/Grid/s/sM2_grid.txt
 
-./Test /home/qiamking/Github/Trivial_Dummy/Output/Grid/s/sM3_grid.txt
+#./Test /home/qiamking/Github/Trivial_Dummy/Output/Grid/s/sM3_grid.txt
+
+###########################
+
+#./Test /home/qiamking/Github/Trivial_Dummy/Output/Grid_Clean/s/sM1_grid.txt
+
+#./Test /home/qiamking/Github/Trivial_Dummy/Output/Grid_Clean/s/sM2_grid.txt
+
+#./Test /home/qiamking/Github/Trivial_Dummy/Output/Grid_Clean/s/sM3_grid.txt
